5aa3475f201970745e260c1151d17c4f56b45aad
Jacob Parker <Jacob.Parker@D2L.com>
2015-10-21 13:38:08 -0400

    Add extension methods for IEnumerable<Claim> Originally from https://git.dev.d2l/projects/AUTH/repos/d2l.security.oauth2/browse/D2L.Security.OAuth2/Utilities/IEnumerableClaimExtensions.cs?at=b83d51baaa590d9c42c8d7f88a758bf3a5a094b5&raw

---
+++ D2L.Services.Core.Extensions/System.Collections.Generic.IEnumerableClaim.Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace D2L.Services {
+	public static partial class DotNetExtensions {
+		/// <summary>
+		/// Determines if a claim matching <paramref name="name"/> exists
+		/// </summary>
+		/// <param name="this"></param>
+		/// <param name="name">The name of the claim to search for</param>
+		/// <returns>True if a matching claim exists, otherwise false</returns>
+		public static bool HasClaim( this IEnumerable<Claim> @this, string name ) {
+			return @this.Any( c => c.Type == name );
+		}
+
+		/// <summary>
+		/// Gets the claim matching <paramref name="name"/> if it exists
+		/// </summary>
+		/// <param name="this"></param>
+		/// <param name="name">The name of the claim to search for</param>
+		/// <param name="value">The value of the claim</param>
+		/// <returns>True if a matching claim exists, otherwise false</returns>
+		public static bool TryGetClaim( this IEnumerable<Claim> @this, string name, out string value ) {
+			Claim claim = @this.FirstOrDefault( c => c.Type == name );
+
+			if( claim == null ) {
+				value = null;
+				return false;
+			}
+
+			value = claim.Value;
+			return true;
+		}
+	}
+}
